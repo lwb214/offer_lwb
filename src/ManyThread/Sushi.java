7f6de8193c32ff333de2770d24f8f3532b922d84
lwb214 <12197587+lwb214@user.noreply.gitee.com>
2023-12-06 22:34:18 +0800

    2th commit 2023.12.6

---
+++ src/ManyThread/Sushi.java
@@ -0,0 +1,69 @@
+package ManyThread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Sushi {
+    /*在自助寿司店中，有1个寿司厨师，3个食客，厨师不断地做寿司并放到转盘上，转盘上最多能放10个寿司，一旦放满厨师就会休息10秒然后再次尝试制作放到转盘，
+    *每位食客每秒吃掉一个寿司(不考虑吃饱的情况)，如果转盘上没有寿司就会休息10秒后再次尝试吃.请以面向对象的编程思想，抽象上面的内容为生产者消费者模式编码。
+    *要求使用多线程实现(可以用线程池，也可以不用*/
+    static int zhuanpanMax = 10;
+    static AtomicInteger curNum = new AtomicInteger(0);
+    public static void main(String[] args) throws InterruptedException {
+        Thread cook = new Cook();
+        Thread d1 = new Diners();
+        Thread d2 = new Diners();
+        Thread d3 = new Diners();
+        d1.setName("客人1");d2.setName("客人2");d3.setName("客人2");
+        cook.start();
+        d1.start();
+        d2.start();
+        d3.start();
+    }
+    static class Cook extends Thread{
+        public void make(){
+            curNum.incrementAndGet();
+            System.out.println("做一块寿司,做了"+ curNum.get());
+        }
+        @Override
+        public void run(){
+          while (true){
+              if ( curNum.get() == zhuanpanMax){
+                  try {
+                      System.out.println("转盘上寿司满了,休息10秒" + System.currentTimeMillis());
+                      Thread.sleep(1000 * 10);
+                  } catch (InterruptedException e) {
+                      e.printStackTrace();
+                  }
+              }else {
+                  make();
+              }
+          }
+        }
+    }
+    static class Diners extends Thread{
+        public void eat() throws InterruptedException {
+            System.out.println("吃一块寿司,还剩" + curNum.get() );
+            curNum.decrementAndGet();
+            Thread.sleep(1000);
+        }
+        @Override
+        public void run(){
+            while (true){
+                if (curNum.get() <= 0){
+                        System.out.println("没有寿司了,休息10秒" + System.currentTimeMillis());
+                    try {
+                        Thread.sleep(1000 * 10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                    try {
+                        eat();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
