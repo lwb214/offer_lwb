d1e0444f699b15eee753fc7de0a106a4de937e72
luwenbin <luwenbin@shizhuang-inc.com>
2023-12-16 14:14:21 +0800

    12.15 路文斌 反转字符串优化

--- src/character/ReverseCharacter.java
+++ src/character/ReverseCharacter.java
@@ -3,28 +3,26 @@ package character;
 import java.util.Scanner;
 /*
     反转字符串
-    输入：a b c d e f g
-    输出：g f e d c b a
+    输入：abcdefg
+    输出：gfedcba
  */
 public class ReverseCharacter {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        String[] arr = str.split(" ");
 
-        reverse(arr);
-        for (String s:arr) {
-            System.out.print(s + " ");
-        }
+        StringBuilder sb = reverse(str);
+        System.out.println(sb);
     }
-    static void reverse(String[] arr){
-            int n = arr.length;
+    static StringBuilder reverse(String arr){
+            int n = arr.length();
+            //用StringBuffer编辑字符串
+            StringBuilder sb  =new StringBuilder(arr);
         for (int i = 0,j = n-1; i < n/2; i++,j--) {
-            String temp;
-           temp = arr[i];
-           arr[i] = arr[j];
-           arr[j] = temp;
+            char  temp = arr.charAt(i);
+            sb.setCharAt(i,sb.charAt(j));
+            sb.setCharAt(j,temp);
         }
+        return sb;
     }
 }
