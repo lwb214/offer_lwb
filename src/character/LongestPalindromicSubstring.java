5d84fa9f850a4f4364002d98cc43a287860b1fc3
lwb214 <12197587+lwb214@user.noreply.gitee.com>
2023-12-20 01:01:48 +0800

    路文斌  2023.12.20 最长回文字串

---
+++ src/character/LongestPalindromicSubstring.java
@@ -0,0 +1,35 @@
+package character;
+import java.util.Scanner;
+/*
+最长回文字串
+ */
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int n = str.length();
+        String ans = "";
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                //找到每一个字串
+                if (isPalindromic(str.substring(i,j)) && j-i > max){
+                    max = j-i;
+                    ans = str.substring(i,j);
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+    //判断是否回文串
+    private static boolean isPalindromic(String s){
+        int len = s.length();
+        for (int i = 0; i < len/2; i++) {
+            if(s.charAt(i) != s.charAt(len-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
