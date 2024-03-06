c6a2feadd57c25df155e7d42dfd5c835079f47c3
lwb214 <12197587+lwb214@user.noreply.gitee.com>
2024-03-07 00:17:23 +0800

    反转部分链表

--- src/linkedList/ReverseLinkedList2.java
+++ src/linkedList/ReverseLinkedList2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ReverseLinkedList2 {
     /*
-        反转链表2 ----反转从m到n之间的链表
+        反转链表2 ----反转从m到n之间的链表(m<n)
         输入：LinkedList head,int m,int n
         输出：反转后的链表LinkedList ans
      */
@@ -17,10 +17,16 @@ public class ReverseLinkedList2 {
         }
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            sc.nextLine();
             String str = sc.nextLine();
+//            String str = "2 3 4 5 6 7 8 9";
             String[] strs = str.split(" ");
             LinkedNode head = createLinkedNode(strs);
             printLinkedNode(head);
+            LinkedNode ans = reverseBetween(head,m,n);
+            printLinkedNode(ans);
         }
         public  static LinkedNode createLinkedNode(String[] arr){
             //虚拟哨兵节点
@@ -46,23 +52,26 @@ public class ReverseLinkedList2 {
             System.out.print("null");
             System.out.println();
         }
-        public LinkedNode reverseBetween(LinkedNode head, int m, int n) {
+        public static LinkedNode reverseBetween(LinkedNode head, int m, int n) {
             LinkedNode dummy = new LinkedNode(0);
             dummy.next = head;
+            //维护p，q指针【p为m-1,q为n】
             LinkedNode p = dummy;
             LinkedNode q = dummy;
             for(int i = 0;i < m - 1;i ++) p = p.next;
             for(int i = 0;i < n ;i ++) q = q.next;
 
+            //保存p，q节点的下一节点（为了连接三段链表）
             LinkedNode a = p.next;
             LinkedNode b = q.next;
+            //将mn之间的链表进行反转，利用了三个指针c,d,e
             for(LinkedNode c = p.next,d = c.next;d != b;) {
                 LinkedNode e = d.next;
                 d.next = c;
                 c = d;
                 d = e;
             }
-
+            //最后将三段链表进行连接
             a.next = b;
             p.next = q;
             return dummy.next;
