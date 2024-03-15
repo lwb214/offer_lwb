15b11a93a8b4f39c4184d2978e75761c53f6f70d
lwb214 <12197587+lwb214@user.noreply.gitee.com>
2024-03-15 23:37:10 +0800

    对称二叉树

---
+++ src/character/LongestPublicChar.java
@@ -0,0 +1,20 @@
+package character;
+
+import java.util.Scanner;
+
+/*
+  找寻两个二叉树的最长公共字串
+*/
+public class LongestPublicChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        String answer = longestCommonChar(str1, str2);
+        System.out.println(answer);
+    }
+    public static String longestCommonChar(String str1,String str2){
+        String result = null;
+
+        return result;
+    }}
